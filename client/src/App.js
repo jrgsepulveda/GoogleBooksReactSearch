22aa6e38951a548c7fe46818f83a4dd202aa94bc
Jorge Sepulveda <jrg.sepulveda@gmail.com>
2020-11-22 12:22:04 -0500

    had to do changes to the api because I was getting an error

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,26 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Header from "./components/Header";
-import Wrapper from "./components/Wrapper";
-
-import NoMatch from "./pages/NoMatch";
-import Search from "./pages/Search";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 import Saved from "./pages/Saved";
-
 import "./App.css";
 
+
+
 class App extends Component {
- 
+
+
   render() {
     return (
       <Router>
-        <div>
-          <NavBar />
-          <Header />
-          <Wrapper>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/saved" component={Saved} />
-            <Route exact path="/noMatch" component={NoMatch} />
-          </Wrapper>
-        </div>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/saved" component={Saved} />
+        </Switch>
+      </div>
       </Router>
-    )
-  };
-};
+    );
+  }
+}
 
 export default App;
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,26 +1,64 @@
-import React, { Component } from "react";
-import API from "../utils/API";
-import Results from "../components/Results";
+import React, { Component } from 'react'
+import { Container } from "../components/Grid/Grid";
+import Nav from "../components/Nav/Nav";
+import Jumbotron from "../components/Jumbotron/Jumbotron";
+import API from '../utils/API';
+import SavedList from "../components/SavedList/SavedList";
 
 class Saved extends Component {
+
     state = {
-        savedBooks: [],
+        savedBooks: []
     }
 
-    componentDidMount() {
-        API.savedBooks()
-            .then(savedBooks => this.setState({ savedBooks: savedBooks }))
-            .catch(err => console.error(err));
+    componentDidMount = () => {
+        this.getBooks()
     }
+
+    deleteGoogleBook = currentBook => {
+        API.deleteBook( currentBook.id )
+        .then(res => {
+            console.log("You deleted this book:", res);
+            this.getBooks();
+        })
+        .catch(err => {
+            console.log("This is the error", err);
+        })
+    }
+
+    getBooks = () => {
+        API.getBooks()
+        .then(res => {
+            this.setState({
+                savedBooks: res.data
+            })
+            console.log("This is the res from getBooks", res);
+        })
+        .catch(err => {
+            console.log("This is the error", err);
+        })
+    }
+
 
     render() {
         return (
-            <div className="container">
-                <h2>Saved books</h2>
-                <Results books={this.state.savedBooks} />
+            <div>
+                <Nav />
+                <Container fluid>
+                <Jumbotron />
+                {this.state.savedBooks.length ? (
+                    <SavedList 
+                    bookState={this.state.savedBooks}
+                    deleteGoogleBook={this.deleteGoogleBook}
+                    >
+                    </SavedList>
+                ) : (
+                    <h5>No results to display</h5>
+                )}
+                </Container>
             </div>
         )
     }
 }
 
-export default Saved;
+export default Saved
